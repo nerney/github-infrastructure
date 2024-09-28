2ff7cee87a8ca276344e467116cded0456dad548
Jacob Nerney <nerney@users.noreply.github.com>
2024-09-28 09:23:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+locals {
+  state = "http://localhost:6061/?type=git&repository=https://github.com/nerney/github-infrastructure&ref=tf-state&state=state.json"
+}
+
 terraform {
+  backend "http" {
+    address = local.state
+    lock_address = local.state
+    unlock_address = local.state
+  }
   required_providers {
     github = {
       source  = "integrations/github"
