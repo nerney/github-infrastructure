60a0d9760270e04389310944e0982e2b11bebe84
Jacob Nerney <nerney@users.noreply.github.com>
2024-09-28 09:28:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-locals {
-  state = "http://localhost:6061/?type=git&repository=https://github.com/nerney/github-infrastructure&ref=tf-state&state=state.json"
-}
-
 terraform {
   backend "http" {
-    address = local.state
-    lock_address = local.state
-    unlock_address = local.state
+    address = "http://localhost:6061/?type=git&repository=https://github.com/nerney/github-infrastructure&ref=tf-state&state=state.json"
+    lock_address = "http://localhost:6061/?type=git&repository=https://github.com/nerney/github-infrastructure&ref=tf-state&state=state.json"
+    unlock_address = "http://localhost:6061/?type=git&repository=https://github.com/nerney/github-infrastructure&ref=tf-state&state=state.json"
     username = "user"
     password = "pswd"
   }
