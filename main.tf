39829c290a24cf2f72d2d459b206f6ededec13c5
Jacob Nerney <nerney@users.noreply.github.com>
2024-09-28 02:10:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {}
+
 resource "github_repository" "repo" {
   name        = "github-infrastructure"
   visibility = "public"
